2eed3bfdd644cc0a384914a1594c61384cb5eb15
Yijia Zhou <zhouyijia1996@gmail.com>
2017-09-19 14:44:24 +0800

    update at@ and scihub strategy

--- qqAI.py
+++ qqAI.py
@@ -28,25 +28,23 @@ def onQQMessage(bot, contact, member, content):
 
     elif content.endswith('@pubmed'):
         keyword = strQ2B(content[:-7])
-        ans = 'https://www.ncbi.nlm.nih.gov/pubmed/?term=' + keyword.replace(' ', '+') + '\n'
-        bot.SendTo(contact, ans)
+        url = 'https://www.ncbi.nlm.nih.gov/pubmed/?term=' + keyword.strip().replace(' ', '+')
+        bot.SendTo(contact, url)
         time.sleep(random.choice(range(3, 6)))
 
-        pubmedresult = getPubmed(keyword)
+        pubmedresult = getPubmed(url)
         tosend = ''
         for result_index in range(4):
             try:
                 tosend += next(pubmedresult)+'\n'
             except StopIteration:
-                tosend += 'That\'s all.'
+                tosend += "That's all."
                 break
         bot.SendTo(contact, tosend)
 
     elif content.endswith('@scihub'):
         keyword = strQ2B(content[:-7])
-        pub = next(getPubmed(keyword))
-        PMID = pub.strip()[-8:]
-        bot.SendTo(contact, getScihub(PMID))
+        bot.SendTo(contact, getScihub(keyword))
 
     elif content.endswith('@chat'):
         senten = urllib.parse.quote(strQ2B(content[:-5]))
@@ -58,8 +56,8 @@ def onQQMessage(bot, contact, member, content):
             bot.SendTo(contact, str(e))
 
 
-    elif '@keyanAI' in content:
-        if content.strip()[0] in '123':
+    elif '@ME' in content or '@keyanAI' in content:
+        if content.strip('[@ME] ').strip()[0] in '123':
             news = sciencenews.db.get_science_news(int(content.strip()[0]))
             for new in news:
                 bot.SendTo(contact, new + '\n')
@@ -67,32 +65,32 @@ def onQQMessage(bot, contact, member, content):
         else:
             bot.SendTo(contact, '''你好，我是科研小 AI, 我目前的功能有：
 关键词 + @pubmed 可获得检索结果页面链接和前四条结果
-数字1-3加 @keyanAI 可获得指定数量的生命科学相关新闻
+数字1-3开头并 @我 可获得指定数量的生命科学相关新闻
 论文标题 + @scihub 可获得 scihub 提供的下载链接
 @我获得使用指南，想随便聊聊的话就用@chat结尾吧O(∩_∩)O''')
 
+'''
 @qqbotslot
 def onNewContact(bot, contact, owner):
-    '''
+    ' ''
     # 当新增 好友/群/讨论组/群成员/讨论组成员 时被调用
     # bot     : QQBot 对象
     # contact : QContact 对象，代表新增的联系人
     # owner   : QContact 对象，仅在新增 群成员/讨论组成员 时有效，代表新增成员所在的 群/讨论组
-    '''
+    '' '
     print('New Contact: ' + str(owner))
-    bot.SendTo(contact, '''你好，我是科研小 AI, 我目前的功能有：
+    bot.SendTo(contact, ' ''你好，我是科研小 AI, 我目前的功能有：
 关键词 + @pubmed 可获得检索结果页面链接和前四条结果
 数字1-3加 @keyanAI 可获得指定数量的生命科学相关新闻
 论文标题 + @scihub 可获得 scihub 提供的下载链接
-@我获得使用指南，想随便聊聊的话就用@chat结尾吧O(∩_∩)O''')
+@我获得使用指南，想随便聊聊的话就用@chat结尾吧O(∩_∩)O' '')
+'''
 
-
-def getPubmed(keyword):
+def getPubmed(url):
     '''
     获取并解析 pubmed 检索结果，输入关键词，返回生成器。
     '''
-    url = 'https://www.ncbi.nlm.nih.gov/pubmed/?term=' + urllib.parse.quote(keyword)
-    html = requests.get(url.replace(' ', '%20')).text
+    html = requests.get(url).text
 
     soup = BeautifulSoup(html, 'lxml')
 
@@ -102,30 +100,45 @@ def getPubmed(keyword):
                 if p.get('class') == ['title']:
                     yield '%s\nhttps://www.ncbi.nlm.nih.gov%s\n' %(p.a.text, p.a.get('href'))
 
-def getScihub(PMID):
+def getScihub(keyword):
     '''
-    通过 PMID 查 scihub, 返回 scihub 提供的下载链接，若得到 pubmed 页面则继续调用 furtherScihub
+    通过 keyword 查 PMID, 返回 scihub 提供的 'www.ncbi.nlm.nih.gov.sci-hub.cc/pubmed/' 下载链接，
+    并读取 doi 号方便手动在 scihub 输入。
     '''
-    pubhuburl = 'http://www.ncbi.nlm.nih.gov.secure.sci-hub.cc/pubmed/' + PMID
-    respon = requests.get(pubhuburl)
-    if respon.url.startswith('http://www.ncbi.nlm.nih.gov.secure.sci-hub.cc/pubmed/'):
-        try:
-            return furtherScihub(respon)
-        except:
-            return furtherScihub(respon)
-    return pubhuburl
-
-def furtherScihub(respon):
-    '''
-    Parse the pubmed@scihub html, get artical url with doi, return the scihub downloading url.
-    '''
+    # pubhuburl = 'http://www.ncbi.nlm.nih.gov.secure.sci-hub.cc/pubmed/' + PMID
+    # respon = requests.get(pubhuburl)
+    # if respon.url.startswith('http://www.ncbi.nlm.nih.gov.secure.sci-hub.cc/pubmed/'):
+    #     try:
+    #         return furtherScihub(respon)
+    #     except:
+    #         return furtherScihub(respon)
+    # return pubhuburl
+    url = 'https://www.ncbi.nlm.nih.gov/pubmed/?term=' + keyword.strip().replace(' ', '+')
+    try:
+        pub = next(getPubmed(url))
+        PMID = pub.strip()[-8:]
+    except StopIteration:
+        h = requests.get(url)
+        soup = BeautifulSoup(h.text, 'lxml')
+        PMID = soup.findAll('dd')[-2].span.text
+    
+    pubhuburl = 'http://www.ncbi.nlm.nih.gov.sci-hub.cc/pubmed/' + PMID
+    respon = requests.get('http://www.ncbi.nlm.nih.gov/pubmed/' + PMID)
     soup = BeautifulSoup(respon.text, 'lxml')
     doi = soup.findAll('dd')[-1].a.text
-    handle = requests.get('http://doi.org/api/handles/' + doi, timeout=15)
-    puburl = handle.json()['values'][0]['data']['value']
-    puburl = puburl[puburl.index('://')+3:]
-    scihuburl = puburl[:puburl.index('/')] + '.secure.sci-hub.cc' + puburl[puburl.index('/'):]
-    return scihuburl
+    return '{pubhuburl}\n如果点击此链接未成功下载 PDF, 请在 sci-hub.cc 中输入 {doi} 并点击右侧钥匙按钮。'.format(pubhuburl=pubhuburl, doi=doi)
+
+# def furtherScihub(respon):
+#     '''
+#     Parse the pubmed@scihub html, get artical url with doi, return the scihub downloading url.
+#     '''
+    # soup = BeautifulSoup(respon.text, 'lxml')
+    # doi = soup.findAll('dd')[-1].a.text
+    # handle = requests.get('http://doi.org/api/handles/' + doi, timeout=15)
+    # puburl = handle.json()['values'][0]['data']['value']
+    # puburl = puburl[puburl.index('://')+3:]
+    # scihuburl = puburl[:puburl.index('/')] + '.secure.sci-hub.cc' + puburl[puburl.index('/'):]
+#     return scihuburl
 
 def strQ2B(ustring):
     """全角转半角"""
